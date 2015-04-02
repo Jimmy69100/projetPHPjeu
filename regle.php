84b84c29bb0d31363bb55e2a0c669d70360e07d4
lecalut <lecalut@hotmail.fr>
2015-04-03 01:04:24 +0200

    modig

--- regle.php
+++ regle.php
@@ -1,3 +1,11 @@
 <?php include'head.php'; ?>
 <div id="regle">
+<li>
+<p>1. chaque joueur dispose d'une palette de 24 personnages, et d'une carte personnage.</p>
+<p>2. Le but du jeu est de deviner la carte personnage de l'adversaire avant celui-ci</p>
+<p>3. Pour celà, vous devez poser des question à votre adversaire afin d'eliminer le plus vite
+possible les personnage de votre palette afin d'en garder qu'un : le personnage representant
+la carte de l'adversaire.</p>
+<p>4/. Le premier joueur à trouver la carte de l'adversaire remporte la partie.</p>
+</li>
 <?php include'footer.php'; ?>
