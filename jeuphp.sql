b16e09fd80c29eded5f04932110d9551d227333a
clementhenrion <sommak313@laposte.net>
2015-04-05 18:08:37 +0200

    modification de la base de donne

--- jeuphp.sql
+++ jeuphp.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Ven 03 Avril 2015 à 16:53
+-- Généré le :  Dim 05 Avril 2015 à 17:59
 -- Version du serveur :  5.6.15-log
 -- Version de PHP :  5.5.8
 
@@ -23,28 +23,145 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `barbe`
+--
+
+CREATE TABLE IF NOT EXISTS `barbe` (
+  `ID_barbe` int(15) NOT NULL,
+  `couleur` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `type` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `image` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `barbe`
+--
+
+INSERT INTO `barbe` (`ID_barbe`, `couleur`, `type`, `image`) VALUES
+(1, 'gris', 'sans-moustache', 'barbe1.png'),
+(2, 'roux', 'sans-moustache', 'barbe2.png'),
+(3, 'blond', 'sans-moustache', 'barbe3.png'),
+(4, 'noir', 'normal', 'barbe4.png'),
+(5, 'gris', 'normal', 'barbe5.png'),
+(6, 'roux', 'normal', 'barbe6.png'),
+(7, 'noir', 'sans-moustache', 'barbe7.png'),
+(8, 'noir', 'barbiche', 'barbe8.png'),
+(9, 'gris', 'barbiche', 'barbe9.png'),
+(10, 'roux', 'barbiche', 'barbe10.png'),
+(11, 'blond', 'barbiche', 'barbe11.png'),
+(12, 'noir', 'moustache', 'barbe12.png'),
+(13, 'blond', 'moustache', 'barbe13.png'),
+(14, 'gris', 'moustache', 'barbe14.png'),
+(15, 'roux', 'moustache', 'barbe15.png');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `cheveux`
 --
 
 CREATE TABLE IF NOT EXISTS `cheveux` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ID_cheveux` int(18) NOT NULL AUTO_INCREMENT,
   `image` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `couleur` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `genre` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
+  `type` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`ID_cheveux`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=19 ;
 
 --
 -- Contenu de la table `cheveux`
 --
 
-INSERT INTO `cheveux` (`id`, `image`, `couleur`, `genre`) VALUES
-(1, 'cheveux1.png', 'roux', 'femme'),
-(2, 'cheveux2.png', 'brun', 'femme'),
-(3, 'cheveux3.png', 'blond', 'femme'),
-(4, 'cheveux4.png', 'brun', 'homme'),
-(5, 'cheveux5.png', 'blond', 'homme'),
-(6, 'cheveux6.png', 'roux', 'homme');
+INSERT INTO `cheveux` (`ID_cheveux`, `image`, `couleur`, `genre`, `type`) VALUES
+(1, 'cheveux1.png', 'roux', 'femme', 'court'),
+(2, 'cheveux2.png', 'gris', 'femme', 'court'),
+(3, 'cheveux3.png', 'blond', 'femme', 'court'),
+(4, 'cheveux4.png', 'noir', 'femme', 'court'),
+(5, 'cheveux5.png', 'noir', 'femme', 'mi-long'),
+(6, 'cheveux6.png', 'roux', 'femme', 'mi-long'),
+(7, 'cheveux7.png', 'blond', 'femme', 'mi-long'),
+(8, 'cheveux8.png', 'noir', 'femme', 'long'),
+(9, 'cheveux9.png', 'blond', 'femme', 'long'),
+(10, 'cheveux10.png', 'gris', 'femme', 'long'),
+(11, 'cheveux11.png', 'roux', 'femme', 'long'),
+(12, 'cheveux12.png', 'noir', 'homme', 'cour'),
+(13, 'cheveux13.png', 'blond', 'homme', 'cour'),
+(14, 'cheveux14.png', 'roux', 'homme', 'cour'),
+(15, 'cheveux15.png', 'noir', 'homme', 'calvicie'),
+(16, 'cheveux16.png', 'blond', 'homme', 'calvicie'),
+(17, 'cheveux17.png', 'gris', 'homme', 'calvicie'),
+(18, 'cheveux18.png', 'roux', 'homme', 'calvicie');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `collier`
+--
+
+CREATE TABLE IF NOT EXISTS `collier` (
+  `ID_collier` int(8) NOT NULL,
+  `couleur` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `type` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `image` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `collier`
+--
+
+INSERT INTO `collier` (`ID_collier`, `couleur`, `type`, `image`) VALUES
+(1, 'bleu', 'long', 'collier1.png'),
+(2, 'jaune', 'long', 'collier2.png'),
+(3, 'rouge', 'long', 'collier3.png'),
+(4, 'gris', 'long', 'collier4.png'),
+(5, 'rouge', 'court', 'collier5.png'),
+(6, 'bleu', 'court', 'collier6.png'),
+(7, 'jaune', 'court', 'collier7.png'),
+(8, 'gris', 'court', 'collier8.png');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `corp`
+--
+
+CREATE TABLE IF NOT EXISTS `corp` (
+  `ID_corp` int(2) NOT NULL,
+  `couleur` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `image` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `corp`
+--
+
+INSERT INTO `corp` (`ID_corp`, `couleur`, `image`) VALUES
+(1, 'blanc', 'blanc.png'),
+(2, 'noir', 'noir.png');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `lunettes`
+--
+
+CREATE TABLE IF NOT EXISTS `lunettes` (
+  `ID_lunettes` int(5) NOT NULL,
+  `couleur` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `image` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `lunettes`
+--
+
+INSERT INTO `lunettes` (`ID_lunettes`, `couleur`, `image`) VALUES
+(1, 'vert', 'lunette1.png'),
+(2, 'violet', 'lunette2.png'),
+(3, 'bleu', 'lunette3.png'),
+(4, 'noir', 'lunette4.png'),
+(5, 'orange', 'lunette5.png');
 
 -- --------------------------------------------------------
 
@@ -69,6 +186,40 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
 
 INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `pseudo`, `mail`, `mdp`, `date_inscription`) VALUES
 (3, 'gauthe', 'jimmy', 'admin', 'admin@admin.gmail', '04d98d2819faf945261d3b827ba4c12a65c36405', '1/04/15');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `vetement`
+--
+
+CREATE TABLE IF NOT EXISTS `vetement` (
+  `ID_vetement` int(1) NOT NULL,
+  `couleur` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `type` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `image` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `vetement`
+--
+
+INSERT INTO `vetement` (`ID_vetement`, `couleur`, `type`, `image`) VALUES
+(1, 'bleu', 'normal', 'vetement1.png'),
+(2, 'vert', 'normal', 'vetement2.png'),
+(3, 'noir', 'normal', 'vetement3.png'),
+(4, 'rouge', 'normal', 'vetement4.png'),
+(5, 'bleu', 'col-monte', 'vetement5.png'),
+(6, 'marron', 'col-monte', 'vetement6.png'),
+(7, 'vert', 'col-monte', 'vetement7.png'),
+(8, 'rouge', 'col-monte', 'vetement8.png'),
+(9, 'marron', 'col-large', 'vetement9.png'),
+(10, 'bleu', 'cravate', 'vetement10.png'),
+(11, 'vert', 'cravate', 'vetement11.png'),
+(12, 'rouge', 'cravate', 'vetement12.png'),
+(13, 'violet', 'cravate', 'vetement13.png'),
+(14, 'bleu', 'col-large', 'vetement14.png'),
+(15, 'violet', 'col-large', 'vetement15.png');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
