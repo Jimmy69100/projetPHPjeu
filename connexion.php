5878a36656f8fadb82e39953f88e47cce0637328
lecalut <lecalut@hotmail.fr>
2015-04-12 13:40:27 +0200

    zone mot de passe

--- connexion.php
+++ connexion.php
@@ -3,7 +3,7 @@
 <a href="index.php" id="home"><img src="accueil/home.png"></a>
 <form id="connex" action="v_connexion.php" method="POST">
 	<p>Votre pseudo :<input type="text" name="pseudo"/></p>
-	<p>Votre mot de passe :<input type="pass" name="mdp"/></p>
+	<p>Votre mot de passe :<input type="password" name="mdp"/></p>
 	<input id="valide" type="submit" value="Je valide"/>
 
 </form>
