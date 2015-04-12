37dd9bf2dbb201e4942a7767827ba106c14b7a93
lecalut <lecalut@hotmail.fr>
2015-04-12 14:22:39 +0200

    modif champ

--- connexion.php
+++ connexion.php
@@ -4,7 +4,7 @@
 <form id="connex" action="v_connexion.php" method="POST">
 	<p>Votre pseudo :<input type="text" name="pseudo"/></p>
 	<p>Votre mot de passe :<input type="password" name="mdp"/></p>
-	<input id="valide" type="submit" value="Je valide"/>
+	<input id="valide" type="image" src="accueil/connexion.png"/>
 
 </form>
 <?php include'footer.php'; ?>
