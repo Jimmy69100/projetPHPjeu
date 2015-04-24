c00cfd90e0183d26b3ffca96eb01698fbcc2692d
jimmy <jimmy.gauthe@gmail.com>
2015-04-24 20:35:34 +0200

    (grosse) MAJ bonhomme

--- jeuphp (2).sql
+++ jeuphp (2).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Ven 24 Avril 2015 à 17:41
+-- Généré le :  Ven 24 Avril 2015 à 20:35
 -- Version du serveur :  5.6.15-log
 -- Version de PHP :  5.5.8
 
@@ -30,8 +30,9 @@ CREATE TABLE IF NOT EXISTS `barbe` (
   `ID_barbe` int(15) NOT NULL,
   `couleur` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `type` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `image` varchar(50) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `image` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`ID_barbe`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Contenu de la table `barbe`
@@ -67,7 +68,7 @@ CREATE TABLE IF NOT EXISTS `cheveux` (
   `genre` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `type` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`ID_cheveux`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=19 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=19 ;
 
 --
 -- Contenu de la table `cheveux`
@@ -103,14 +104,16 @@ CREATE TABLE IF NOT EXISTS `collier` (
   `ID_collier` int(8) NOT NULL,
   `couleur` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `type` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `image` varchar(50) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `image` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`ID_collier`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Contenu de la table `collier`
 --
 
 INSERT INTO `collier` (`ID_collier`, `couleur`, `type`, `image`) VALUES
+(0, 'aucun', 'aucun', ''),
 (1, 'bleu', 'long', 'collier1.png'),
 (2, 'jaune', 'long', 'collier2.png'),
 (3, 'rouge', 'long', 'collier3.png'),
@@ -129,8 +132,9 @@ INSERT INTO `collier` (`ID_collier`, `couleur`, `type`, `image`) VALUES
 CREATE TABLE IF NOT EXISTS `corp` (
   `ID_corp` int(2) NOT NULL,
   `couleur` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `image` varchar(50) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `image` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`ID_corp`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Contenu de la table `corp`
@@ -149,8 +153,9 @@ INSERT INTO `corp` (`ID_corp`, `couleur`, `image`) VALUES
 CREATE TABLE IF NOT EXISTS `lunettes` (
   `ID_lunettes` int(5) NOT NULL,
   `couleur` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `image` varchar(50) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `image` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`ID_lunettes`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Contenu de la table `lunettes`
@@ -162,6 +167,94 @@ INSERT INTO `lunettes` (`ID_lunettes`, `couleur`, `image`) VALUES
 (3, 'bleu', 'lunette3.png'),
 (4, 'noir', 'lunette4.png'),
 (5, 'orange', 'lunette5.png');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `nom`
+--
+
+CREATE TABLE IF NOT EXISTS `nom` (
+  `ID_nom` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `genre` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `pris` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID_nom`),
+  KEY `pris` (`pris`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=49 ;
+
+--
+-- Contenu de la table `nom`
+--
+
+INSERT INTO `nom` (`ID_nom`, `nom`, `genre`, `pris`) VALUES
+(1, 'antoine', 'homme', 0),
+(2, 'cedric', 'homme', 0),
+(3, 'jeremy', 'homme', 0),
+(4, 'damien', 'homme', 0),
+(5, 'anthony', 'homme', 0),
+(6, 'serge', 'homme', 0),
+(7, 'robert', 'homme', 0),
+(8, 'marc', 'homme', 0),
+(9, 'idriss', 'homme', 0),
+(10, 'patrick', 'homme', 0),
+(11, 'pascal', 'homme', 0),
+(12, 'antonin', 'homme', 0),
+(13, 'marcel', 'homme', 0),
+(14, 'bernard', 'homme', 0),
+(15, 'david', 'homme', 0),
+(16, 'etienne', 'homme', 0),
+(17, 'françois', 'homme', 0),
+(18, 'gerald', 'homme', 0),
+(19, 'hector', 'homme', 0),
+(20, 'ludovic', 'homme', 0),
+(21, 'olivier', 'homme', 0),
+(22, 'quentin', 'homme', 0),
+(23, 'yves', 'homme', 0),
+(24, 'pierre', 'homme', 0),
+(25, 'yvette', 'femme', 0),
+(26, 'anais', 'femme', 0),
+(27, 'sophia', 'femme', 0),
+(28, 'marilyn', 'femme', 0),
+(29, 'murielle', 'femme', 0),
+(30, 'esmeralda', 'femme', 0),
+(31, 'josianne', 'femme', 0),
+(32, 'elise', 'femme', 0),
+(33, 'celine', 'femme', 0),
+(34, 'cynthia', 'femme', 0),
+(35, 'julia', 'femme', 0),
+(36, 'samantha', 'femme', 0),
+(37, 'sandrine', 'femme', 0),
+(38, 'morgane', 'femme', 0),
+(39, 'beatrice', 'femme', 0),
+(40, 'camille', 'femme', 0),
+(41, 'gwen', 'femme', 0),
+(42, 'maria', 'femme', 0),
+(43, 'nadine', 'femme', 0),
+(44, 'lucie', 'femme', 0),
+(45, 'olivia', 'femme', 0),
+(46, 'tabatha', 'femme', 0),
+(47, 'victoria', 'femme', 0),
+(48, 'amelia', 'femme', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `personnage`
+--
+
+CREATE TABLE IF NOT EXISTS `personnage` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ID_corp` int(11) NOT NULL,
+  `ID_cheveux` int(11) NOT NULL,
+  `ID_barbe` int(11) DEFAULT NULL,
+  `ID_collier` int(11) DEFAULT NULL,
+  `ID_lunettes` int(11) DEFAULT NULL,
+  `ID_vetement` int(11) NOT NULL,
+  `Id_nom` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ID_corps` (`ID_corp`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=66 ;
 
 -- --------------------------------------------------------
 
@@ -197,8 +290,9 @@ CREATE TABLE IF NOT EXISTS `vetement` (
   `ID_vetement` int(1) NOT NULL,
   `couleur` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `type` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `image` varchar(50) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `image` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`ID_vetement`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Contenu de la table `vetement`
--- nouvellepartie2.php
+++ nouvellepartie2.php
@@ -1,18 +1,29 @@
 <?php include'head.php'; 
 
+$i = 1;
+
+while ($i <25) {
+
+
+
+
+
 //affichage du corps noir/blanc
 $corps = "SELECT * FROM corp ORDER BY RAND() LIMIT 0, 1";
 $corps = $bdd->query($corps);
 while ($donnee = $corps->fetch() ) {
 ?>
 	<img src="Corps/<?php echo $donnee['image']; ?>" /></br>
+
 <?php
+$id_corps = $donnee['ID_corp'];
 }
-$corps->closeCursor();
+
 
 
 //affichage des cheveux aleatoire homme/femme
-
+$id_barbe = "";
+$id_collier = "";
 
 $cheveux = "SELECT * FROM cheveux ORDER BY RAND() LIMIT 0, 1";
 $cheveux = $bdd->query($cheveux);
@@ -22,24 +33,31 @@ while ($cheveux2 = $cheveux->fetch() ) {
 	<?php
 $genre = $cheveux2['genre'];
 $couleur = $cheveux2['couleur'];
+$id_cheveux = $cheveux2['ID_cheveux'];
 
 }
 
 //condition, on sait la couleur des poils :D et le genre, donc gogo:
 
-
+if ($genre == 'homme') {
 $barbe = "SELECT * FROM barbe WHERE couleur = :couleur ORDER BY RAND() LIMIT 0, 1";
 $barbe = $bdd->prepare($barbe);
 $barbe->execute(array(
     'couleur' => $couleur
     ));
 
-if ($genre == 'homme') {
+
 while ($barbe2= $barbe->fetch() ) {
 ?>
 	<img src="Barbe/<?php echo $barbe2['image']; ?>" /></br>
 	<?php
+	$id_barbe = $barbe2['ID_barbe'];
 									}
+
+
+
+
+
 }
 
 else {
@@ -51,7 +69,13 @@ while ($collier2= $collier->fetch() ) {
 ?>
 	<img src="Collier/<?php echo $collier2['image']; ?>" /></br>
 	<?php
+	$id_collier = $collier2['ID_collier'];
 									}
+
+
+
+
+
 }
 
 $lunettes = "SELECT * FROM lunettes ORDER BY RAND() LIMIT 0, 1";
@@ -60,6 +84,7 @@ while ($lunettes2= $lunettes->fetch() ) {
 ?>
 	<img src="Lunette/<?php echo $lunettes2['image']; ?>" /></br>
 	<?php
+	$id_lunette = $lunettes2['ID_lunettes'];
 									}
 
 $vetement = "SELECT * FROM vetement ORDER BY RAND() LIMIT 0, 1";
@@ -68,12 +93,51 @@ while ($vetement2= $vetement->fetch() ) {
 ?>
 	<img src="vetement/<?php echo $vetement2['image']; ?>" /></br>
 	<?php
+	$id_vetement = $vetement2['ID_vetement'];
 									}
 
+$nom = "SELECT * FROM nom WHERE pris = :pris AND genre = :genre ORDER BY RAND() LIMIT 0, 1";
+$nom = $bdd->prepare($nom);
+$nom->execute(array(
+	'pris' => 0,
+	'genre' => $genre
+	));
+while ($nom2= $nom->fetch() ) {
+	echo $nom2['nom'];
+	$id_nom = $nom2['ID_nom'];
+									
+$nom_update = "UPDATE nom SET pris = :pris WHERE nom = :nom";
+$nom_update = $bdd->prepare($nom_update);
+$nom_update->execute(array(
+	'nom' => $nom2['nom'],
+	'pris' => 1
+	));
+
+}
+
+$insertion = $bdd->prepare('INSERT INTO personnage(ID_corp, ID_cheveux, ID_barbe, ID_collier, ID_lunettes, ID_vetement, ID_nom) VALUES(:corp, :cheveux, :barbe, :collier, :lunette, :vetement, :nom)');
+$insertion->execute(array(
+	'corp' => $id_corps,
+	'cheveux' => $id_cheveux,
+	'barbe' => $id_barbe,
+	'collier' => $id_collier,
+	'lunette' => $id_lunette,
+	'vetement' => $id_vetement,
+	'nom' => $id_nom
+	));
+
+
+$i = $i + 1;
+
+}
 
 
 
-
+$nom_update2 = "UPDATE nom SET pris = :pris";
+$nom_update2 = $bdd->prepare($nom_update2);
+$nom_update2->execute(array(
+	'pris' => 0
+	));
 
 
 ?>
