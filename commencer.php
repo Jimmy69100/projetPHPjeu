d6b8498d2cd7b0b2efcb3c459a8aba1ff8c5147c
Damien <yorlin@hotmail.fr>
2015-04-28 00:02:38 +0200

    Connard

--- commencer.php
+++ commencer.php
@@ -1,7 +1,6 @@
 <?php include'head.php'; ?>
 <div id="head">
 <li><a href="nouvellepartie.php"><img src="accueil/Nouvellepartie.png"></a><br></li>
-<li><a href="rejoindrepartie.php"><img src="accueil/Rejoindreunepartie.png"></a><br></li>
 <li><a href="mespartieencours.php"><img src="accueil/Partiesencours.png"></a><br></li>
 <li><a href="moncompte.php"><img src="accueil/Compte.png"></a><br></li>
 <?php include'footer.php'; ?>
