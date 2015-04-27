a53795f85d33e7bcc9bc484198cd7c30d3ede941
Damien <yorlin@hotmail.fr>
2015-04-27 19:47:15 +0200

    Jimmy le batard

--- regle.php
+++ regle.php
@@ -1,11 +1,11 @@
 <?php include'head.php'; ?>
 <div id="regle">
 <li>
-<p>1. chaque joueur dispose d'une palette de 24 personnages, et d'une carte personnage.</p>
-<p>2. Le but du jeu est de deviner la carte personnage de l'adversaire avant celui-ci</p>
-<p>3. Pour cel&agrave;, vous devez poser des question &agrave; votre adversaire afin d'eliminer le plus vite
-possible les personnage de votre palette afin d'en garder qu'un : le personnage representant
+<p>1. Chaque joueur dispose d'une palette de 24 personnages, et d'une carte personnage.</p>
+<p>2. Le but du jeu est de deviner la carte personnage de l'adversaire avant ce dernier</p>
+<p>3. Pour cel&agrave;, vous devez poser des question &agrave; votre adversaire afin d'&eacuteliminer le plus vite
+possible les personnages de votre palette afin d'en garder qu'un : le personnage repr&eacutesentant
 la carte de l'adversaire.</p>
-<p>4. Le premier joueur &agrave; trouver la carte de l'adversaire remporte la partie.</p>
+<p>4. Le premier joueur &agrave; trouver la carte de l'adversaire remporte la partie !</p>
 </li>
 <?php include'footer.php'; ?>
