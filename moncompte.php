14461dade207f76a58d80c304bd745aeaca0a922
Damien <yorlin@hotmail.fr>
2015-04-27 23:14:58 +0200

    EvgenyMafia

--- moncompte.php
+++ moncompte.php
@@ -1,8 +1,8 @@
 <?php include'head.php'; ?>
 <div id="head">
-<h1>Bienvenue dans votre interface de gestion d'utilisateur</h1>
+<h1 id="bienvenue">Bienvenue dans votre interface de gestion d'utilisateur</h1>
  <p>Vous pouvez a partir de cette interface modifier vos information de compte, le nombre de parties
-  joué </p></br>
+  jou&eacute </p></br>
 
 
     <h3>Vos informations : </h3>
@@ -15,21 +15,21 @@
   while ($donne = $req->fetch() ) {
     echo "Votre pseudo : ", $donne['pseudo'], "</br>";
     echo "votre adresse mail : ", $donne['mail'], "</br>";
-    echo "Votre nombre de partie gagn&ée : ", $donne['nb_partie_gagnee'], "</br>";
+    echo "Votre nombre de partie gagn&eacutee : ", $donne['nb_partie_gagnee'], "</br>";
     echo "Votre nombre de partie perdu : ", $donne['nb_partie_perdu'], "</br>";
   }
   $req->closeCursor();
 ?>
 
-<h3>Modification des infos</h3>
-<p>changer votre adresse mail : </p>
+<h3>Modification des informations</h3>
+<p>Changer votre adresse mail : </p>
 <form method="post" action="nouveau_mail.php">
    <input type="email" name="mail" placeholder="entrez votre nouveau mail" size="35"></br></br>
     <input type="submit" value="enregistrement">
 
 </form>
 
-<p>changer votre mot de passe : </p>
+<p>Changer votre mot de passe : </p>
 <form method="post" action="nouveau-pass.php">
    <input type="password" name="pass" placeholder="entrez votre nouveau mot de passe" size="35"></br></br>
     <input type="submit" value="enregistrement">
