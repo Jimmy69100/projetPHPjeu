f72f8837e1150737089b2ee1a9d63f83d841bb1b
lecalut <lecalut@hotmail.fr>
2015-04-27 09:14:36 +0200

    connexion

--- connexion.php
+++ connexion.php
@@ -1,8 +1,8 @@
 <?php include'head.php'; ?>
 <div id="connexion">
 <form id="connex" action="v_connexion.php" method="POST">
-	<p>Votre pseudo :<input type="text" name="pseudo"/></p>
-	<p>Votre mot de passe :<input type="password" name="mdp"/></p>
+	<p>Votre pseudo :<input type="text" name="pseudo" required/></p>
+	<p>Votre mot de passe :<input type="password" name="mdp" required/></p>
 	<input id="valide" type="image" src="accueil/connexion.png"/>
 
 </form>
